09695477165f8ec7a0d52a6a71e063d41cec6400
Aimar Roosalu <aimar.roosalu@gmail.com>
2021-03-16 22:35:03 +0200

    Testin main klassis faili kirjutajat

--- src/Peaklass.java
+++ src/Peaklass.java
@@ -1,9 +1,11 @@
 import java.io.File;
+import java.io.FileWriter;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Peaklass {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
 
         // testin faili lugejat
         File file = new File("src/RiskigruppideKlassifikaator.txt");
@@ -11,5 +13,12 @@ public class Peaklass {
         while(scan.hasNextLine()) {
             System.out.println(scan.nextLine());
         }
+
+        // testin faili kirjutajat
+        String failiSisu = "10.04.2021";
+        FileWriter writer = new FileWriter("src/VaktsineerimiseNimekiri.txt");
+        writer.write(failiSisu);
+        writer.close();
+
     }
 }
