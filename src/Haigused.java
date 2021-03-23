ed0dda39221c251b513a21224c460d935c4834e8
Aimar Roosalu <aimar.roosalu@gmail.com>
2021-03-23 18:10:51 +0200

    Väike uuendus ja puhastus

--- src/Haigused.java
+++ src/Haigused.java
@@ -1,31 +1,33 @@
-/* siit tõmbame failist kõik haigused */
-
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Haigused {
 
-    // isendiväljad
     String haiguseNimetus; // Isendiväli (hiljem tuleb siia list) haiguste nimekirja jaoks
-    String failiAsukoht = "src/RiskigruppideKlassifikaator.txt"
+    String failiAsukoht = "src/RiskigruppideKlassifikaator.txt";
 
     // konstruktor
 
-    public Haigused(String haiguseNimetus) {
+    public Haigused(String haiguseNimetus, String failiAsukoht) {
         this.haiguseNimetus = haiguseNimetus;
+        this.failiAsukoht = failiAsukoht;
     }
 
-    // meetodid
+    public Haigused(String astma) {
+    }
 
-    // POOLELI!!! loen sisse failist haigused listi (see tuleb teha funktsiooniks, mis väljastab haiguse)
-    ArrayList<String> haigused = new ArrayList<>();
-    File failiSisu = new File(failiAsukoht);
+    // meetod haiguste saamiseks listi
+
+    public ArrayList haigusedFailist(failiAsukoht) {
+        ArrayList<String> haigused = new ArrayList<>();
+        File failiSisu = new File(failiAsukoht);
         try (Scanner sc = new Scanner(failiSisu, "UTF-8")) {
-        while(sc.hasNextLine()) {
-            haigused.add(sc.nextLine()); // haigused lisatud failist listi
+            while (sc.hasNextLine()) {
+                haigused.add(sc.nextLine());
+            }
         }
+        return haigused;
     }
-        // return true or false (kas see haigus asub loetelus).
 
 }
